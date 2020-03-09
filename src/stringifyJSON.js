34a781824b0c6e7474bf29408827e04518eef8e1
Rayen RAFFA <raffa.rayen@gmail.com>
2020-03-09 14:16:46 +0100

    add stop conditions for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,38 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  switch(typeof(obj)) {
+  	case 'number':
+  		return obj;
+  		break;
+  	case 'string':
+  		return obj;
+  		break;
+  	case 'boolean':
+  		return obj;
+  		break;
+  	case 'undefined':
+  		return null;
+  		break;
+  	case 'function':
+  		return null
+  		break;
+  	case 'symbol':
+  		return null;
+  		break;
+  	case 'date':
+  		return obj;
+  		break;
+  	case 'NaN':
+  	case 'null':
+  	case 'Infinity':
+  		return null;
+  		break;
+  	case 'object':
+  		if (Array.isArray(obj)) {
+  			// Array processing here
+  		}
+  }
+
+
 };
